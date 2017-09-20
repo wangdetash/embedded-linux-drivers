7b425ca9a589fcacfdedaf5b168eb647f498bc2c
wangdetash <wangdetash@gmail.com>
2017-09-20 11:04:39 +0530

    program for passing integer and string into kernel module

--- passing_commandline_arguments/passing_arg.c
+++ passing_commandline_arguments/passing_arg.c
@@ -1,4 +1,4 @@
-//program for passing string into kernel module
+//program for passing integer and string into kernel module
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
@@ -7,15 +7,19 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tashi George");
 
-int i;
+int myint;
 char *mystring;
+
+module_param(myint, int, 0);
+MODULE_PARM_DESC(myint, "An integer");
 module_param(mystring, charp, 0000);
 MODULE_PARM_DESC(mystring, "A character string");
 
 int init_module(void)
 {
 printk(KERN_ALERT "Hello, world \n=============\n");
-printk(KERN_ALERT "mystring is a string: %s\n", mystring);
+printk(KERN_INFO "myint is an integer: %d\n", myint);
+printk(KERN_INFO "mystring is a string: %s\n", mystring);
 return 0;
 }
 
